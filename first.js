cf9ed5abbe74f17f966b2fe65e8d26cb56abc312
SHAHIL ROY <shahilroy360@gmail.com>
2025-09-18 22:14:46 +0530

    Added banner image styling and fixed CSS

--- first.js
+++ first.js
@@ -1,1 +1,16 @@
-// Arithmetic Operators
+// // Arithmetic Operators
+// let a = 10;
+// let b = 5;
+
+// console.log("a + b =", a+b);
+
+// // Assignment operators
+// a = 5;
+// a **= 4; 
+// console.log("a = ", a);
+
+// // Comparison Operators
+let a = 5;
+let b = 2;
+
+console.log("a == b", a == b);
